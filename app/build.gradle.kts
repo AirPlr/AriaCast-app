1fb2d519930ae86a144c8ab9a6844a37c856058d
Lorenzo <lorenzoimbastari@gmail.com>
2026-07-20 15:38:40 +0200

    fix and repub

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.aria.ariacast"
         minSdk = 31
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.4"
+        versionCode = 13
+        versionName = "1.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
